30dd39b51122378203bcdb0f3973f664df737129
mitya.kulikbk.ru <mitya.kulik@bk.ru>
2023-03-21 00:44:33 +0300

    Подключил и протестил прилежение с БД

--- src/main/java/com/example/iShop/models/Product.java
+++ src/main/java/com/example/iShop/models/Product.java
@@ -1,20 +1,38 @@
 package com.example.iShop.models;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Entity
+@Table(name = "products")
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
     private Long id;
+    @Column(name = "name")
     private String name;
+    @Column(name = "description", columnDefinition = "text")
     private String description;
+    @Column(name = "organization")
     private String organization;
+    @Column(name = "price")
     private  int price;
+    @Column(name = "quantityStorage")
     private int quantityStorage;
+    @Column(name = "discountInformation")
     private String discountInformation;
+    @Column(name = "comments")
     private String comments;
+    @Column(name = "keyWords")
     private String keyWords;
+    @Column(name = "characteristicsTable")
     private String characteristicsTable;
+    @Column(name = "score")
     private int score;
 }
